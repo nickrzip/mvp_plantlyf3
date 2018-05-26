d7a76c181b5e5b1b91dddbe64caa85116643745c
nickrzip <rogers.a.nick@gmail.com>
2018-05-26 16:22:46 -0400

    osdfoiansdfnasdfoinsaodi

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.json());
 app.use('/hist', histRouter);
 app.use('/users', userRouter);
 
-port = process && process.env.port || 8080;
+port = process.env.port || 8080;
 
 app.listen(port, () => {
     console.log(`listening on ${port}`);
